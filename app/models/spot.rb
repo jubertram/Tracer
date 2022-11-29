3c4d4b5dcbc6172214bdc04c1fd0523591f95d36
Julien Bertram <bertramjulien@gmail.com>
2022-11-29 17:38:00 +0100

    map added

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -4,4 +4,7 @@ class Spot < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :spot_moves, dependent: :destroy
   has_many :moves, through: :spot_moves
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
