f48763c2803c052bd2077a6e532a64749a606b53
gabriel <rises-426@hotmail.fr>
2022-11-30 21:46:48 +0100

    spot controller et profile page

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,4 +1,6 @@
 class SpotsController < ApplicationController
+  # before_action :set_spot, only: :destroy
+  before_action :set_partner, only: [:show, :destroy]
   def index
     @spots = Spot.all
     # The `geocoded` scope filters only spots with coordinates
@@ -12,18 +14,34 @@ class SpotsController < ApplicationController
   end
 
   def show
-
   end
 
   def new
-
+    @spot = Spot.new
   end
 
   def create
-
+    @spot = Spot.new(spot_params)
+    if @spot.save
+      redirect_to spot_path(@spot)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @spot.destroy
+    redirect_to spot_path(@spot), status: :see_other
+  end
 
+  private
+
+
+  def set_spot
+    @spot = Spot.find(params[:id])
+  end
+
+  def spot_params
+    params.require(:spot).permit(:spot_difficulty, :address, :photo_url)
   end
 end
