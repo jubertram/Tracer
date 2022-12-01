45d68bcc39d944be0e7e1480372241b04c2e6b37
Axel LoganMolbert <axldarealest@gmx.com>
2022-12-01 15:27:27 +0100

    btn bookmark

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -5,6 +5,12 @@ class BookmarksController < ApplicationController
   end
 
   def create
-
+    spot = Spot.find(params[:spot_id])
+    @bookmarks = Bookmark.new(user_id: current_user.id, spot_id: spot.id)
+    if @bookmarks.save
+      redirect_to spot_path(spot)
+    else
+      redirect_to spot_path(spot), alert: 'Something went wrong.'
+    end
   end
 end
